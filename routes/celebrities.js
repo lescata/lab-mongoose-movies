8f6ce914bf51df3d9281a45d353c4fb8f6d1a9fb
lescata <alexandre.lescat@gmail.com>
2022-07-09 12:37:52 +0200

    iteration 5

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -17,4 +17,51 @@ router.get("/celebrities", function (req, res, next) {
     });
 });
 
+router.get("/celebrities/new", function (req, res, next) {
+  res.render("celebrities/new", {});
+});
+
+router.post("/celebrities/new", function (req, res, next) {
+  const { name, occupation, catchPhrase } = req.body;
+  console.log(name, occupation, catchPhrase);
+  Celebrity.create({
+    name: name,
+    occupation: occupation,
+    catchPhrase: catchPhrase,
+  })
+    .then(function (celebritiesFromDB) {
+      // { _id: '', title: , description:  }
+      console.log("enregistrement creee", celebritiesFromDB);
+
+      // res.send("ok");
+      res.redirect("/celebrities");
+    })
+    .catch((err) => {
+      console.log("oops, create d un livre", err);
+      next(err);
+    });
+});
+
+router.post("/celebrities/:id/remove", function (req, res, next) {
+  Celebrity.findByIdAndRemove(req.params.id)
+    .then(function () {
+      res.redirect("/celebrities");
+    })
+    .catch((err) => {
+      console.log(err);
+      next(err);
+    });
+});
+
+router.get("/celebrities/:id", (req, res, next) => {
+  Celebrity.findById(req.params.id)
+    .then((celebrityFromDB) => {
+      res.render("celebrities/show", celebrityFromDB);
+    })
+    .catch((err) => {
+      console.log(err);
+      next(err);
+    });
+});
+
 module.exports = router;
